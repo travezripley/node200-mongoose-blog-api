178d21c927b7f362fa3ebd6d3b808b4b874a72c6
Travis Ripley <theripleys@gmail.com>
2019-05-06 19:20:18 -0700

    updats to users.js

--- server/app.js
+++ server/app.js
@@ -2,19 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-mongoose.connect('mongodb://localhost/my-blog', { useMongoClient: true });
+mongoose.connect('mongodb://localhost:27017/node200-mongoose-blog-api', { useMongoClient: true });
 mongoose.Promise = Promise;
 
 const app = express();
 const morgan = require('morgan');
 
-app.use(bodyParser.json());
 app.use(morgan('dev'));
-app.use('/api/users', require('./routes/users'));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     console.log("whats good player")
     res.status(200).send("hello");
 });
+
+app.use('/api/users', require('./routes/users'));
 
 module.exports = app;
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,11 +1,92 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/User"); //gets the folder - users Schema
 
-router.get("/", (req, res) => {
-  User.find().then(users => {
-    res.status(200).json(users);
+// "Get all Users"
+router.get("/", (req, res, next) => {
+  User.find()
+    .exec()
+    .then(users => {
+      console.log(users);
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// "Get Single User"
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  User.findById(id)
+    .then(user => {
+      console.log("user:", user);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).send("error: user not found");
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+// "Create a User"
+router.post("/", (req, res, next) => {
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email
   });
+
+  user
+    .save()
+    .then(result => {
+      console.log(result);
+    })
+    .catch(err => console.log(err));
+  res.status(201).json({
+    message: "User was Created"
+  });
+});
+
+// "Update a User"
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  User.findByIdAndUpdate(id, {
+    $set: {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email
+    }
+  })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(204).send(result);
+    })
+    .catch(err => console.log(err));
+});
+
+// "Delete a User"
+router.delete("/", (req, res) => {
+  const id = req.params.id;
+  User.findByIdAndRemove(id)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 });
 
 module.exports = router;
