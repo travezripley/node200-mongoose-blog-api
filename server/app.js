76974cd4f815d4e1d04f75f2934297408d6a669c
Travis Ripley <theripleys@gmail.com>
2019-05-08 10:51:12 -0700

    routes

--- server/app.js
+++ server/app.js
@@ -15,8 +15,9 @@ app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 
-app.use("/api/users", require("./routes/users"));
-app.use("/api/blogs", require("./routes/blogs"));
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/blogs', require('./routes/blogs'));
 
 app.get("/", (req, res) => {
   res.status(200).send();
